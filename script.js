3265801cc8ea34d5a6971f7712bcc65f53e930db
katarzynakaz <l453r3@gmail.com>
2025-04-20 13:24:49 +0100

    200

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// helpers
+//global vars
+let allEpisodes = []; //so i can access in other functions
+const searchBox = document.querySelector("#searchBox");
+//to hide counterDiv by default
+const counterDiv = document.querySelector(".counterDiv");
+
+// 100
+// // helpers
 const formatSE = (episode) => {
   // if (episodeSeason < 10 || episodeNumber < 10) no need pad is auto {
   const seasonStr = episode.season.toString().padStart(2, "0"); // Fixed: episodeSeason -> episode.season
@@ -17,6 +24,14 @@ const formatSE = (episode) => {
           />
           <p class="episodeDescription"></p>
         </div`;
+
+`episodeCard = {
+          name: 
+          season: 
+          number: 
+          summary:
+        }
+          create episodeCode using se and num;`;
 
 const generateCard = (episode) => {
   const episodeCard = document.createElement("div");
@@ -65,8 +80,77 @@ function makePageForEpisodes(episodeList) {
 
 //do not change getAllEpisodes function
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  allEpisodes = getAllEpisodes(); //edited to use for filter
   makePageForEpisodes(allEpisodes);
 }
+
+///////////////////////////////
+// 200
+const findButton = document.querySelector("#findButton");
+
+// Search functionality
+const findEpisode = () => {
+  //to get access to allEpisodes
+  // setup(); to access allEpisodes, but it didnt wokr so I made a global on top
+  // grab user text
+  const userSearchTerm = document.querySelector("#searchInput").value;
+
+  // filter shown based on title including the search term
+  //taken from allEpisodes
+  const filteredEpisodes = allEpisodes.filter((episode) => {
+    //this was surprisingly frustrating
+    return episode.name.toLowerCase().includes(userSearchTerm.toLowerCase());
+  });
+
+  // show the count to user
+  showCountToUser(filteredEpisodes);
+
+  // reload to see
+  makePageForEpisodes(filteredEpisodes);
+
+  // clear user input
+  userSearchTerm.value = "";
+};
+
+// helper - display counts to user
+const showCountToUser = (filteredEpisodes) => {
+  const howManyMatch = document.querySelector("#howManyMatch");
+  const howManyInTotal = document.querySelector("#howManyInTotal");
+
+  // Display the number of matches and total episodes
+  howManyMatch.innerHTML = filteredEpisodes.length;
+  howManyInTotal.innerHTML = allEpisodes.length;
+
+  //adjacentHTML was not woking for me so instead removing hidden by default class
+  //children to append to searchBox, using adjacentHTML instead of placing in vars to append child
+  // searchBox.insertAdjacentHTML(
+  //   "beforeend",
+  //   `
+  //   <p id="resultCounter">Showing</p>
+  //   <p id="howManyMatch">${filteredEpisodes.length}</p>
+  //   <p>/</p>
+  //   <p id="howManyInTotal">${allEpisodes.length}</p>
+  // `
+
+  //remove hidden class from counterHidden
+  counterDiv.classList.remove("hidden");
+};
+
+// event listener for button
+findButton.addEventListener("click", findEpisode);
+
+//////////////////////////////////////////////////////////
+//live search - tried impementing but it was not working after maaany attempts and
+// I have a search and clear buttons and a filter instead task-200
+
+const useClearButton = document.querySelector("#clearButton");
+
+useClearButton.addEventListener("click", () => {
+  document.querySelector("#searchInput").value = "";
+  //add hidden class to counterDiv
+  counterDiv.classList.add("hidden");
+  makePageForEpisodes(allEpisodes);
+  showCountToUser(allEpisodes);
+});
 
 window.onload = setup;
